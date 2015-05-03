d0e5ac53f2b1a131cfacff633e238cf2c8b255f7
JuanPablo <jpabloaj@gmail.com>
2015-05-03 19:19:38 +0000

    filter with tag name
    * imgur tag_name : return a random image tagged with the tag
      called tag_name.

--- app.py
+++ app.py
@@ -6,28 +6,46 @@ from flask import jsonify
 from flask import render_template
 import os
 import random
+from utils import tag_from_text
 
 from imgurpython import ImgurClient
+from imgurpython.helpers.error import ImgurClientError
 
 app = Flask(__name__)
 
 
-def get_imgur_image():
+def random_link_from_gallery_list(gallery_list):
+    random_gallery_index = random.randrange(len(gallery_list))
+    return gallery_list[random_gallery_index].link
+
+
+def get_imgur_image(text):
     client_id = os.environ.get('IMGUR_CLIENT_ID')
     client_secret = os.environ.get('IMGUR_CLIENT_SECRET')
 
     if client_id and client_secret:
 
         client = ImgurClient(client_id, client_secret)
-        gallery = client.gallery()
-        random_gallery_index = random.randrange(len(gallery))
-        return gallery[random_gallery_index].link
+        tag = tag_from_text(text)
+
+        if tag:
+            try:
+                gallery = client.gallery_tag(tag)
+                gallery = gallery.items
+            except ImgurClientError:
+                return 'Tag {} not found'.format(tag)
+        else:
+
+            gallery = client.gallery()
+
+        return random_link_from_gallery_list(gallery)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def return_imgur_image():
     if request.method == 'POST':
-        req = {'text': get_imgur_image()}
+        text = request.form['text']
+        req = {'text': get_imgur_image(text)}
         return jsonify(**req)
     elif request.method == 'GET':
         return render_template('index.html')
